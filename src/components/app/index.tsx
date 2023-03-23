71c2d6613a1ec85876930ce755867f9a0eb2c537
KuroNoDev <kuronogiasu@gmail.com>
2023-03-23 23:34:30 +0900

    pwd

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,15 +1,27 @@
+import React from 'react'
 import { Hero } from '../hero'
 import { Nav } from '../nav'
 import { Ui } from '../ui'
 import styles from './styles.module.scss'
 
 function App() {
+  const [enabled, setEnabled] = React.useState(false);
+
+  React.useEffect(() => {
+    const password = prompt('cutest person in the world?')
+    if (password === 'marlamarlamarla') {
+      setEnabled(true);
+    }
+  }, [])
+
   return (
+    enabled ?
     <div className={styles.app}>
       <Nav />
       <Hero />
       <Ui />
-    </div>
+    </div> :
+    <div />
   )
 }
 
